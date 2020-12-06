52e909a89d136cd0c070d35df16a490daf7921d9
ellysiann <elenvital135@gmail.com>
2020-12-06 22:27:44 +0330

    task3

---
+++ New folder/src/Components/HomePage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components"
+
+const Header = styled.div`
+    width: 100%;
+    background-color: #32BEA6;  
+    height: 87px;
+`
+const Body = styled.div`
+
+`
+
+export default function HomePage() {
+    return(
+        <>
+            <Body>
+                <Header />
+            </Body>
+        </>
+    )
+}
